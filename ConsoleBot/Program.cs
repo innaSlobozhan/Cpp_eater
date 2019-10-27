557e0b04b649677ff2f19f12254897ce98c7e9c1
Шудря Виталий <vitallik22@hotmail.com>
2019-10-27 17:00:27 +0200

    Исправил проблему с кнопкой и подправил Program

--- ConsoleBot/Program.cs
+++ ConsoleBot/Program.cs
@@ -10,21 +10,23 @@ namespace ConsoleBot
     {
         #region Vars
         static string[] Lectures = { "http://telegra.ph/October-2-2014-01-22", "http://www.telegraph.co.uk/news/worldnews/europe/italy/7223365/Silvio-Berlusconi-says-immigrants-not-welcome-but-beautiful-girls-can-stay.html" };
-        static InlineKeyboardMarkup inlineKeyboardAnswers = new InlineKeyboardMarkup( new InlineKeyboardButton[][]
+
+        static InlineKeyboardMarkup inlineKeyboardOK = new InlineKeyboardMarkup(new InlineKeyboardButton() { Text = "Почати тестування", CallbackData = "OK"});
+        static InlineKeyboardMarkup urlButton = new InlineKeyboardMarkup(new InlineKeyboardButton() { Text = "Проект на  Github", Url = "https://github.com/innaSlobozhan/Cpp_eater", CallbackData = "URL"});
+
+        static ReplyKeyboardMarkup replyKeyboard = new ReplyKeyboardMarkup(new KeyboardButton[][]
         {
-            new InlineKeyboardButton[]
+            new KeyboardButton[]
             {
-                new InlineKeyboardButton(){Text = "1", CallbackData = "1"},
-                new InlineKeyboardButton(){Text = "2", CallbackData = "2"}
+                new KeyboardButton(){Text = "1"},
+                new KeyboardButton(){Text = "2"}
             },
-            new InlineKeyboardButton[]
+            new KeyboardButton[]
             {
-                new InlineKeyboardButton(){Text = "3", CallbackData = "3"},
-                new InlineKeyboardButton(){Text = "4", CallbackData = "4"}
+                new KeyboardButton(){Text = "3"},
+                new KeyboardButton(){Text = "4"}
             }
         });
-        static InlineKeyboardMarkup inlineKeyboardOK = new InlineKeyboardMarkup(new InlineKeyboardButton() { Text = "Почати тестування", CallbackData = "OK"});
-        static InlineKeyboardMarkup urlButton = new InlineKeyboardMarkup(new InlineKeyboardButton() { Text = "Проект на  Github", Url = "https://github.com/innaSlobozhan/Cpp_eater", CallbackData = "URL"}); 
 
         static List<User> users = new List<User>();
         
@@ -35,45 +37,52 @@ namespace ConsoleBot
             Client = new TelegramBotClient("1050115445:AAFMHnHtznwJNdwZtQ_J0kKF6iqZ8K9EnX4");
             XMLmanager.ReadFile(users);
 
-            Client.OnMessage += FirstCommand;
+            Client.OnMessage += Command_Handler;
             Client.OnCallbackQuery += Keyboard_Handler;
             Client.StartReceiving();
             Console.ReadKey(true);
             Client.StopReceiving();
         }
 
-        static void FirstCommand(object Sender, MessageEventArgs e)
+        static void Command_Handler(object Sender, MessageEventArgs e)
         {
             User currentUser = users.Find(user => user.ChatID == e.Message.Chat.Id);
-            if (e.Message.Text == "/start")
+
+            #region Commands
+            switch (e.Message.Text)
             {
-                Client.SendTextMessageAsync(currentUser.ChatID, "Для виклику лекцій використовуйте команду /study. Уважно прочитайте її вміст і приступайте до тестів, в випадку успішного проходження (мінімум 4 тести з 5) Ви отримуєте доступ до наступної лекції.");
-                ExtensionList.Add(users, new User(e.Message.Chat.Id));
+                case "/start":
+                    Client.SendTextMessageAsync(currentUser.ChatID, "Для виклику лекцій використовуйте команду /study. Уважно прочитайте її вміст і приступайте до тестів, в випадку успішного проходження (мінімум 4 тести з 5) Ви отримуєте доступ до наступної лекції.");
+                    ExtensionList.Add(users, new User(e.Message.Chat.Id));
+                    break;
+                case "/study":
+                    Client.SendTextMessageAsync(currentUser.ChatID, Lectures[currentUser.Level], replyMarkup: inlineKeyboardOK);
+                    currentUser.Manager.SetXFile("Tests\\Test" + currentUser.Level + ".xml");
+                    currentUser.Manager.ReadTest();
+                    break;
+                case "/showlectures":
+                    string @string = "";
+                    for (int i = 0; i <= currentUser.Level; i++)
+                    {
+                        @string += $"{i + 1}. " + Lectures[i] + "\n";
+                    }
+                    Client.SendTextMessageAsync(currentUser.ChatID, @string);
+                    break;
+                case "/reset":
+                    currentUser.Level = 0;
+                    Client.SendTextMessageAsync(currentUser.ChatID, "Повернення до початкового рівня.");
+                    break;
+                case "/donate":
+                    Client.SendTextMessageAsync(currentUser.ChatID, "Ви можете оцінити наш проект або почати стежити за ним за наступним посиланням.", replyMarkup: urlButton);
+                    break;
+                case "1": case "2" :case "3": case "4":
+                    currentUser.Manager.Examination.TakeAnswer(Client, currentUser, replyKeyboard, Convert.ToInt32(e.Message.Text));
+                    break;
+                default:
+                    Client.SendTextMessageAsync(currentUser.ChatID, "Невідома команда!");
+                    break;
             }
-            else if (e.Message.Text == "/study")
-            {
-                Client.SendTextMessageAsync(currentUser.ChatID, Lectures[currentUser.Level], replyMarkup: inlineKeyboardOK);
-                currentUser.Manager.SetXFile("Tests\\Test" + currentUser.Level + ".xml");
-                currentUser.Manager.ReadTest();
-            }
-            else if (e.Message.Text == "/showlectures")
-            {
-                string @string = "";
-                for (int i = 0; i <= currentUser.Level; i++)
-                {
-                   @string += $"{i + 1}. " + Lectures[i] + "\n";
-                }
-                Client.SendTextMessageAsync(currentUser.ChatID, @string);
-            }
-            else if (e.Message.Text == "/reset")
-            {
-                currentUser.Level = 0;
-                Client.SendTextMessageAsync(currentUser.ChatID, "Повернення до початкового рівня.");
-            }
-            else if(e.Message.Text == "/donate")
-            {
-                Client.SendTextMessageAsync(currentUser.ChatID, "Ви можете оцінити наш проект або почати стежити за ним за наступним посиланням.", replyMarkup: urlButton);
-            }
+            #endregion
         }
 
         static void Keyboard_Handler(object sender, CallbackQueryEventArgs e)
@@ -83,17 +92,12 @@ namespace ConsoleBot
             if(e.CallbackQuery.Data == "OK")
             {
                 Client.AnswerCallbackQueryAsync(e.CallbackQuery.Id);
-                currentUser.Manager.Examination.ShowTest(Client, currentUser.ChatID, inlineKeyboardAnswers);
+                currentUser.Manager.Examination.ShowTest(Client, currentUser.ChatID, replyKeyboard);
 
             }
             else if(e.CallbackQuery.Data == "URL")
             {
                 Client.AnswerCallbackQueryAsync(e.CallbackQuery.Id);
-            }
-            else
-            {
-                Client.AnswerCallbackQueryAsync(e.CallbackQuery.Id);
-                currentUser.Manager.Examination.TakeAnswer(Client, currentUser, inlineKeyboardAnswers, Convert.ToInt32(e.CallbackQuery.Data));
             }
         }
     }
--- ConsoleBot/TestApi/Exam.cs
+++ ConsoleBot/TestApi/Exam.cs
@@ -17,7 +17,7 @@ namespace ConsoleBot.TestApi
         }
 
 
-        public void ShowTest(TelegramBotClient bot, long ChatId, InlineKeyboardMarkup keyboard)
+        public void ShowTest(TelegramBotClient bot, long ChatId, ReplyKeyboardMarkup keyboard)
         {
             try
             {
@@ -28,36 +28,36 @@ namespace ConsoleBot.TestApi
 
             }
         }
-        public void TakeAnswer(TelegramBotClient bot, User currentUser, InlineKeyboardMarkup keyboard, int answer)
+        public void TakeAnswer(TelegramBotClient bot, User currentUser, ReplyKeyboardMarkup keyboard, int answer)
         {
             if(testPos <= tests.Count - 1)
             {
                 if (tests[testPos].GetAnswer()[answer - 1])
                     answerCount++;
                 testPos++;
-                ShowTest(bot, currentUser.ChatID, keyboard);
-            }
-            else
-            {
-                if(answerCount >= tests.Count - 1)
+                if(testPos > tests.Count - 1)
                 {
+                    if (answerCount >= tests.Count - 1)
+                    {
 
-                    bot.SendTextMessageAsync(currentUser.ChatID, $"Ви успішно пройшли тест!\nВи відповіли на {answerCount} питань з {tests.Count}.");
-                    answerCount = 0;
-                    testPos = 0;
+                        bot.SendTextMessageAsync(currentUser.ChatID, $"Ви успішно пройшли тест!\nВи відповіли на {answerCount} питань з {tests.Count}.", replyMarkup: new ReplyKeyboardRemove());
+                        answerCount = 0;
+                        testPos = 0;
 
-                    if (currentUser.Level < 10)
-                        currentUser.LevelUp();
+                        if (currentUser.Level < 10)
+                            currentUser.LevelUp();
 
-                    XMLmanager.UpdateLevel(currentUser);
+                        XMLmanager.UpdateLevel(currentUser);
+                    }
+                    else if (answerCount < tests.Count - 1)
+                    {
+                        bot.SendTextMessageAsync(currentUser.ChatID, $"Ви не пройшли тест! Визвіть команду /study\nВи відповіли на {answerCount} питань з {tests.Count}.", replyMarkup: new ReplyKeyboardRemove());
+                        answerCount = 0;
+                        testPos = 0;
+                    }
                 }
-                else if(answerCount < tests.Count - 1)
-                {
-                    bot.SendTextMessageAsync(currentUser.ChatID, $"Ви не пройшли тест! Визвіть команду /study\nВи відповіли на {answerCount} питань з {tests.Count}.");
-                    answerCount = 0;
-                    testPos = 0;
-                }
-                    
+                else
+                    ShowTest(bot, currentUser.ChatID, keyboard);
             }
         }
     }
